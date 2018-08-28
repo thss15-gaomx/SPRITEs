b09b9dbce4846255591f1ec11933573d7bbfe3cc
gmx <thss15_gaomx@163.com>
2018-08-28 05:22:43 -0700

    add voice assitant

--- js/sound.js
+++ js/sound.js
@@ -1,110 +1,66 @@
 var DROP_SOUND = new buzz.sound([
     "/static/drop.mp3"
 ]);
-var LINE_SOUND = new buzz.sound([
-    "/static/line.mp3"
-]);
-var TETRIS_SOUND = new buzz.sound([
-    "/static/tetris.mp3"
-]);
 var MOVE_SOUND = new buzz.sound([
     "/static/move.mp3"
 ]);
 var ROTATE_SOUND = new buzz.sound([
     "/static/rotate.mp3"
 ]);
-var GAMEOVER_SOUND = new buzz.sound([
-    "/static/gameover.mp3"
-]);
-var THEME_SOUND = new buzz.sound([
-    "/static/theme.mp3"
+var OVERLAP_SOUND = new buzz.sound([
+    "/static/overlap.mp3"
 ]);
 
-var GROUP_SOUND = new buzz.group([ DROP_SOUND, LINE_SOUND, TETRIS_SOUND, MOVE_SOUND, ROTATE_SOUND, GAMEOVER_SOUND, THEME_SOUND ]);
+var GROUP_SOUND = new buzz.group([ DROP_SOUND, MOVE_SOUND, ROTATE_SOUND, OVERLAP_SOUND ]);
 
-function isAvailableSound() { 
+function isAvailableSound() {
 	return !($("#sound").css("display") === "none");
 }
-function loadAllSound() { 
+function loadAllSound() {
 	if ( isAvailableSound() ) GROUP_SOUND.load();
 }
-function stopAllSound() { 
-	if (isAvailableSound()) { 
+function stopAllSound() {
+	if (isAvailableSound()) {
 		GROUP_SOUND.stop();
 	}
 }
-function muteAllSound() { 
-	if (isAvailableSound()) { 
+function muteAllSound() {
+	if (isAvailableSound()) {
 		GROUP_SOUND.mute();
 	}
 }
-function unmuteAllSound() { 
-	if (isAvailableSound()) { 
+function unmuteAllSound() {
+	if (isAvailableSound()) {
 		GROUP_SOUND.unmute();
 	}
 }
 
-function playDropSound() { 
-	if ( isAvailableSound() ) { 
+function playDropSound() {
+	if ( isAvailableSound() ) {
 		ROTATE_SOUND.stop();
 		MOVE_SOUND.stop();
 		DROP_SOUND.stop();
 		DROP_SOUND.play();
 	}
 }
-function playLineSound() { 
-	if ( isAvailableSound() ) { 
-		ROTATE_SOUND.stop();
-		MOVE_SOUND.stop();
-		LINE_SOUND.play();
-	}
-}
-function playTetrisSound() { 
-	if ( isAvailableSound() ) { 
-		ROTATE_SOUND.stop();
-		MOVE_SOUND.stop();
-		TETRIS_SOUND.play();
-	}
-}
-function playMoveSound() { 
-	if ( isAvailableSound() ) { 
+function playMoveSound() {
+	if ( isAvailableSound() ) {
 		MOVE_SOUND.stop();
 		MOVE_SOUND.play();
 	}
 }
-function playRotateSound() { 
-	if ( isAvailableSound() ) { 
+function playRotateSound() {
+	if ( isAvailableSound() ) {
 		ROTATE_SOUND.stop();
 		ROTATE_SOUND.play();
 	}
 }
-
-function playGameoverSound() { 
-	if ( isAvailableSound() ) { 
-		GAMEOVER_SOUND.play();
-	}
-}
-function stopGameoverSound() { 
-	if ( isAvailableSound() ) { 
-		GAMEOVER_SOUND.stop();
-	}
-}
-
-function playThemeSound() { 
-	if ( isAvailableSound() ) { 
-		THEME_SOUND.loop();
-		THEME_SOUND.play();
-	}
-}
-function pauseThemeSound() { 
-	if ( isAvailableSound() ) { 
-		LINE_SOUND.stop();
-		TETRIS_SOUND.stop();
-		THEME_SOUND.pause();
-	}
-}
-function stopThemeSound() { 
-	if ( isAvailableSound() ) { 
-		THEME_SOUND.stop();
+function playOverlapSound() {
+	if ( isAvailableSound() ) {
+    ROTATE_SOUND.stop();
+		MOVE_SOUND.stop();
+		DROP_SOUND.stop();
+    OVERLAP_SOUND.stop();
+		OVERLAP_SOUND.play();
 	}
 }
