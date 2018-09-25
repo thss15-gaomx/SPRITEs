e3645020aa63e31fe653435b90355e6bc70ce240
gmx <thss15_gaomx@163.com>
2018-09-25 01:14:53 -0700

    help pages 1-4

--- index/urls.py
+++ index/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^help5$', views.help5, name='help5'),
+    url(r'^help4$', views.help4, name='help4'),
+    url(r'^help3$', views.help3, name='help3'),
+    url(r'^help2$', views.help2, name='help2'),
+    url(r'^help1$', views.help1, name='help1'),
     url(r'^$', views.index, name='index'),
 ]
--- index/views.py
+++ index/views.py
@@ -2,3 +2,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+
+def help1(request):
+    return render(request, "help1.html")
+
+
+def help2(request):
+    return render(request, "help2.html")
+
+
+def help3(request):
+    return render(request, "help3.html")
+
+
+def help4(request):
+    if request.method == 'POST':
+        column = request.POST.get('column')
+        row = request.POST.get('row')
+        return render(request, "help4.html", {'column': int(column) - 1, 'row': int(row) - 1})
+    else:
+        return render(request, "help4.html", {'column': -1, 'row': -1})
+
+def help5(request):
+    return render(request, "help5.html")
